79c16ecda40729a58cfee7627217bbb235e4ddf1
KGahan <kimgahan05@gmail.com>
2022-04-29 15:42:00 +0200

    Added Endpoints

--- PersonenOrt.Repository.Service/Controllers/PersonController.cs
+++ PersonenOrt.Repository.Service/Controllers/PersonController.cs
@@ -25,9 +25,19 @@ namespace PersonenOrt.Repository.Service.Controllers
             }
         }
         [HttpPut("{id:int}")]
-        public Person PutPerson(int id, Person person)
-        {
-            return null;
+        public String PutPerson(int id, Person person)
+        { 
+            using(var context = new PersonenOrtContext())
+            {
+                var PersonToBeDeleted = context.Person.FirstOrDefault(p => p.Id == id);
+                if (PersonToBeDeleted == null)
+                    return "Person with id " + id + "not found";
+
+                context.Person.Remove(PersonToBeDeleted);
+                context.Person.Add(person);
+                context.SaveChanges();
+            }
+            return "Person with id " + id + " has been updated";
         }
 
         [HttpDelete("{id:int}")]
